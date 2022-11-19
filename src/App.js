a444086c33c5ca81f5c2fe0bceb1ac4db8616598
Eben-Success <aebenezer237@gmail.com>
2022-11-19 22:45:07 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1> {title} </h1>
+        <p>Liked {likes} times </p>
+
+        <p> [10] </p>
+        <p> {"Hello, ninjas"} </p>
+        <p> {Math.random() * 10} </p>
+        <a href="{link}">Google Site</a>
       </div>
     </div>
   );

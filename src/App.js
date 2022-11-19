a6914c1f45b25782a274e3605cfabcdb85d66381
Eben-Success <aebenezer237@gmail.com>
2022-11-19 22:52:51 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from './Home';
 
 function App() {
   const title = "Welcome to the new blog";
@@ -7,14 +9,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1> {title} </h1>
-        <p>Liked {likes} times </p>
-
-        <p> [10] </p>
-        <p> {"Hello, ninjas"} </p>
-        <p> {Math.random() * 10} </p>
-        <a href="{link}">Google Site</a>
+       <Home />
       </div>
     </div>
   );

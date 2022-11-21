45e8a062c1a03d4dfcf3c23e9cf339d5af331961
Eben-Success <aebenezer237@gmail.com>
2022-11-21 05:17:12 +0000

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,8 +6,7 @@ const Navbar = () => {
             <h1>The Success Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-
+                <a href="/create" >New Blog</a>
             </div>
             </nav>
      );

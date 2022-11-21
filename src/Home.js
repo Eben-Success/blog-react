b28500292292aab21826a1a481abe9f3c3e026c6
Eben-Success <aebenezer237@gmail.com>
2022-11-21 10:14:44 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,35 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch(' http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
+        setTimeout(() => {
+                fetch(' http://localhost:8000/blogs')
+                    .then(res => {
+                        console.log(res)
+                         if (!res.ok) {
+                            throw Error('could not fetch the data for that resource')
+                        }
+                        return res.json();
+                    })
+                    .then(data => {
+                        setBlogs(data);
+                        setIsPending(false);
+                        setError(null)
+                    })
+                    .catch(err => {
+                        setIsPending(false);
+                        setError(err.message);
             })
-            .then(data => {
-                setBlogs(data)
-            });
+        }, 1000);
     }, []);
   return (
-    <div className="home">
+      <div className="home">
+          {error && <div>{ error }</div>}
+          { isPending && <div>Loading...</div>}
           {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
     </div>
   );

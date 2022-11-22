722498ce9c027b793394e0a5b752e6e68394dc94
Eben-Success <aebenezer237@gmail.com>
2022-11-22 08:28:06 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import Navbar from "./Navbar";
+import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from "./Create";
-import BlogDetails from "./BlogDetails";
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.google.com";
-
   return (
     <Router>
-    <div className="App">
-      <Navbar />
+      <div className="App">
+        <Navbar />
         <div className="content">
           <Switch>
-            <Route exact path="/" > 
-                <Home />
+            <Route exact path="/">
+              <Home />
             </Route>
-            <Route exact path="/create">
+            <Route path="/create">
               <Create />
             </Route>
-            <Route exact path="/blogs/:id">
+            <Route path="/blogs/:id">
               <BlogDetails />
             </Route>
           </Switch>
+        </div>
       </div>
-      </div>
-      </Router>
+    </Router>
   );
 }
 
--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,20 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form >
+                <label >Blog title: </label>
+                <input type="text" required />
+                <label >Blog body: </label>
+                <textarea
+                required>
+                </textarea>
+                <label >Blog author: </label>
+                <select>
+                    <option value="eben">Eben</option>
+                    <option value="sister">sister</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }

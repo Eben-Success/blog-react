a70d70d0986a75c28078148a5924ac34a567cb02
Eben-Success <aebenezer237@gmail.com>
2022-11-22 07:25:55 +0000

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
 // simple functional component
 
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Success Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" >New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" >New Blog</Link>
             </div>
             </nav>
      );

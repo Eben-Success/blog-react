a4dd47253b83dfbb7a2942e5f6592a2f1cdd2ddd
Eben-Success <aebenezer237@gmail.com>
2022-11-22 05:29:22 +0000

    update

---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+// Creating a custom hook
+
+import { useEffect } from "react"
+
+const useFetch = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+    setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                console.log(res)
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+    }, 1000);
+}, []);
+}

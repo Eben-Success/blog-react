579d1915fb62d9a089bb3fde40301135a22ec0b2
Eben-Success <aebenezer237@gmail.com>
2022-11-22 09:29:46 +0000

    done

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -26,7 +26,7 @@ const BlogDetails = () => {
                         </h2>
                         <p>Written by {blog.author}</p>
                         <div>{ blog.body }</div>
-                   <button onClick={} >Delete</button>
+                   <button onClick={handleClick} >Delete</button>
                 </article>
             )}
         </div>
